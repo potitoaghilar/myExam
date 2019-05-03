483d544e19fe93b7e79d51092f0e9ccc9c0b475d
aghilarn <aghilarn@gmail.com>
2019-05-03 23:35:34 +0200

    Preparing UI for final API

--- api/questions.php
+++ api/questions.php
@@ -20,8 +20,7 @@ $answer = new Answer;
 $outputquestions = [];
 
 // Creo la connessione con il db
-$connect = new mysqli('localhost:3306','root','Poti1997$','esame');
-//$connect = new mysqli('localhost:8889','root','root','esame');
+$connect = new mysqli('localhost:8889','root','root','esame');
 
 // Eseguo la query per prelevare le domande
 $getquestions = $connect->query("Select * from questions");
