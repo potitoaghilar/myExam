8f01af8f888bd1da8c3f3c326f3ce9c52dd6d416
Marco Rizzi <rizzi97@hotmail.com>
2019-05-04 14:22:41 +0200

    Update time.php

--- api/time.php
+++ api/time.php
@@ -1,5 +1,20 @@
 <?php
+header("Content-Type: application/json; charset=UTF-8");
+$matricola=568254;
+$questions=30; //define questions number
+$timequestion=60; //secondi
+$maxtime=$questions*$timequestion;
 
-echo json_encode([
-    'time' => 30 * 60,
-]);
+$connect = new mysqli('localhost:8889','root','root','esame');
+
+$getTime = $connect->query("SELECT TIME_TO_SEC(TIMEDIFF(NOW(), start)) seconds from users where matricola=".$matricola);
+
+while($gettedtime = $getTime->fetch_array()){
+	
+	$time->time=$gettedtime['seconds'];
+}
+$time->time=$maxtime-$time->time;
+echo json_encode($time); //time in seconds
+
+
+?>
