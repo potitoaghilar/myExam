bf551f606bebecb3f7bf4f67bfb1b3787025f482
Marco Rizzi <rizzi97@hotmail.com>
2019-05-04 14:52:21 +0200

    Update publish.php

--- api/publish.php
+++ api/publish.php
@@ -1,4 +1,5 @@
 <?php
+header("Content-Type: application/json; charset=UTF-8");
 //$matricola = $_GET['matricola'];
 //$risultato= $_GET['results'];
 //$nome =$_GET['nome'];
@@ -7,10 +8,14 @@ $connessione = new mysqli('localhost:8889','root','root','esame');
 $matricola= 568254;
 $id_answer=4;
 $id_question = 4;
-print "INSERT INTO users_answers (matricola_user, id_answer, id_question) VALUES('".$matricola."','".$id_answer."','".$id_question."') ON DUPLICATE KEY UPDATE id_answer='".$id_question."';";
-$query = $connessione->query("INSERT INTO users_answers (matricola_user, id_answer, id_question) VALUES('".$matricola."','".$id_answer."','".$id_question."') ON DUPLICATE KEY UPDATE id_answer='".$id_question."';");;
-//$query = $connessione->query("insert into results (matricola,punteggio,nome,cognome) values ('".$matricola."','".$risultato."','".$nome."','".$cognome."')");
-//print "insert into results (matricola,punteggio) values ('".$matricola."','".$risultato."')";
-$connessione->close();
-//print '<div class="success">successo</div>';
+
+$query = $connessione->query("INSERT INTO users_answers (matricola_user, id_answer, id_question) VALUES('".$matricola."','".$id_answer."','".$id_question."') ON DUPLICATE KEY UPDATE id_answer='".$id_question."';");
+if($query){
+	$response->success="success";
+}
+else{
+	$response->success="error";
+}
+	$connessione->close();
+print json_encode($response);
 ?>
