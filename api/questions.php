64b49b4a7a3f9a12b11f5978b68a34d8c44f6194
aghilarn <aghilarn@gmail.com>
2019-05-04 15:55:51 +0200

    Bug fix

--- api/questions.php
+++ api/questions.php
@@ -1,8 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-header("Content-Type: application/json; charset=UTF-8");
+
+require 'requires.php';
 
 // Definisco Classi
 class Answer{
