21db2a04257a260ec7808872ad894daf24874816
Marco Rizzi <rizzi97@hotmail.com>
2019-05-05 21:29:09 +0200

    last

--- www/api/requires/database.php
+++ www/api/requires/database.php
@@ -2,20 +2,11 @@
 
 class Database {
 
-  /*  const config = [
+    const config = [
         'host' => 'localhost',
         'port' => '3306',
         'username' => 'root',
         'password' => 'Poti1997$',
-        'database' => 'esame',
-    ];
-	*/
-
-    const config = [
-        'host' => 'localhost',
-        'port' => '8889',
-        'username' => 'root',
-        'password' => 'root',
         'database' => 'esame',
     ];
 
