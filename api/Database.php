9a744a134e4fa9a1c65c05f8bfec99b04d6f4166
Marco Rizzi <rizzi97@hotmail.com>
2019-05-05 21:10:50 +0200

    force

--- api/Database.php
+++ api/Database.php
@@ -2,21 +2,22 @@
 
 class Database {
 
-    const config = [
+  /*  const config = [
         'host' => 'localhost',
         'port' => '3306',
         'username' => 'root',
         'password' => 'Poti1997$',
         'database' => 'esame',
     ];
+	*/
 
-    /*const config = [
+    const config = [
         'host' => 'localhost',
         'port' => '8889',
         'username' => 'root',
         'password' => 'root',
         'database' => 'esame',
-    ];*/
+    ];
 
     static function getInstance() {
         return new mysqli(self::config['host'] . ':' . self::config['port'], self::config['username'], self::config['password'], self::config['database']);
