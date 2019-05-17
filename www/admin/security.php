9b7ec1cba1809389e1cc5a5a535aad016f154509
Marco Rizzi <rizzi97@hotmail.com>
2019-05-17 09:40:32 +0200

    Documentazione Aggiornanta

--- www/admin/security.php
+++ www/admin/security.php
@@ -1,6 +1,6 @@
 <?php
 
-// Block connection form other clients: localhost only allowed
+// Block connection form other clients: Allown only one IP
 if($_SERVER['REMOTE_ADDR'] != '151.50.24.190') {
     header('HTTP/1.0 403 Forbidden');
     die('Accesso negato!');
